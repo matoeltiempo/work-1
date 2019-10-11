8ea85a425ccde3daf624f0d5ad3dadd023c04a12
matoeltiempo <matoeltiempoav@gmail.com>
2019-10-12 02:11:53 +0300

    + port

---
+++ app.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+
+const { PORT = 3000, BASE_PATH } = process.env;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log('Ссылка на сервер:');
+  console.log(BASE_PATH);
+});
