bc8e319d1bcd0d487659f1ff18f37d26c0c00b7f
matoeltiempo <matoeltiempoav@gmail.com>
2019-10-12 03:28:51 +0300

    cards + users

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-/* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+
+const cards = require('./routes/cards');
+const users = require('./routes/users');
 
 const { PORT = 3000, BASE_PATH } = process.env;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', cards);
+app.use('/', users);
 
 app.listen(PORT, () => {
   console.log('Ссылка на сервер:');
