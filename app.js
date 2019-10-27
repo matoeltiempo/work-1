8ea5edbb5bd7845eb96d35ee450b08530dcaa2ee
matoeltiempo <matoeltiempoav@gmail.com>
2019-10-27 15:39:08 +0300

    temporary id

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ const users = require('./routes/users');
 const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-
 mongoose.connect("mongodb://localhost:27017/mestodb", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
@@ -19,9 +16,21 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
   useFindAndModify: false,
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5db58bd31de2ae1184558ad5',
+  };
+
+  next();
+});
+
 app.use('/', cards);
 app.use('/', users);
+
 app.use((req, res) => {
   res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
 });
