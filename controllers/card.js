26e5d768ee761db5b253fa001773109cb586ce29
matoeltiempo <matoeltiempoav@gmail.com>
2019-10-27 17:38:44 +0300

    controllers card (dislike, like)

--- controllers/card.js
+++ controllers/card.js
@@ -13,8 +13,22 @@ module.exports.createCard = (req, res) => {
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
 };
 
-module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId)
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
 };
