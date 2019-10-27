ddcfe7dbec118b2511a9b322eed75e723ddecbfe
matoeltiempo <matoeltiempoav@gmail.com>
2019-10-27 16:17:06 +0300

    add card controllers

--- controllers/user.js
+++ controllers/user.js
@@ -9,7 +9,13 @@ module.exports.returnsAllUsers = (req, res) => {
 module.exports.returnsUser = (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.message}` }));
+    .catch((err, user) => {
+      if (!user) {
+        res.status(404).send({ message: "Пользователь с таким id не найден" });
+      } else {
+        res.status(500).send({ message: `Произошла ошибка ${err.message}` });
+      }
+    });
 };
 
 module.exports.createUser = (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,8 @@
 const router = require('express').Router();
-const { createUser } = require('../controllers/user');
+const { createUser, returnsUser, returnsAllUsers } = require('../controllers/user');
 
-const users = require('../data/users');
-
-
-router.get('/users', (req, res) => {
-  res.send(users);
-});
-
-router.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  let flag = '';
-  users.forEach((item) => {
-    if (item._id === id) {
-      flag = item;
-    }
-  });
-
-  if (!flag) {
-    res.statusCode = 404;
-    res.send({ "message": "Нет пользователя с таким id" });
-  } else res.send(flag);
-});
-
+router.get('/users', returnsAllUsers);
+router.get('/users/:id', returnsUser);
 router.post('/users', createUser);
 
 module.exports = router;
