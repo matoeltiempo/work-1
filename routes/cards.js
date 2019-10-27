a8031d5bcdff403299e808c18883dd75e2154d75
matoeltiempo <matoeltiempoav@gmail.com>
2019-10-27 17:45:28 +0300

    router card (delete, put)

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { returnsCards, createCard, deleteCard } = require('../controllers/card');
+const { returnsCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/card');
 
 router.get('/cards', returnsCards);
 router.post('/cards', createCard);
 router.delete('/cards/:cardId', deleteCard);
+router.put('/cards/:cardId/likes', likeCard);
+router.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = router;
